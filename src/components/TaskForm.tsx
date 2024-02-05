fbdb5130ec53736717c4637ebc1d731523c248aa
jaal9709 <jaal9709@gmail.com>
2024-02-04 23:06:35 -0500

    Se realizo la conexion con el backend

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -1,7 +1,66 @@
-import React from 'react'
+import { ChangeEvent, FormEvent, useState } from 'react';
+import { createTaskRequest } from '../api/task';
+
 
 export const TaskForm = () => {
+
+  const [task, setTask] = useState({
+    title: "",
+    description: "",
+    done: false,
+  });
+
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => setTask({...task,[e.target.name]: e.target.value});
+
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(task);
+    const res = await createTaskRequest(task);
+    const data = await res.json();
+    console.log(data);
+  }
+
+
   return (
-    <div>TaskForm</div>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input 
+        type="text" 
+        name='title'
+        className='border-2 border-gray-700 p-2 rounded-lg bg-zinc-800 block w-full my-2'
+        placeholder='Escribe un titulo'
+        onChange={handleChange}
+        />
+
+        <textarea name="description" id="" rows={3}
+          className='border-2 border-gray-700 p-2 rounded-lg bg-zinc-800 block w-full my-2'
+          placeholder='Escribe una descripción'
+          onChange={handleChange}
+        >
+
+        </textarea>
+
+        <label htmlFor=""
+          className='inline-flex items-center gap-x-2'
+          >
+          <input type="checkbox" 
+            name='done' 
+            id=''
+            className='h-5 w-5 text-indigo-600'
+            onChange={(e) => setTask({...task, done: !task.done})
+            }
+            />
+          <span>Done</span>
+        </label>
+
+          <button className='bg-indigo-500 px-3 block py-2 w-full'>
+            Save
+          </button>
+
+        
+      </form>
+    </div>
   )
 }
