bab02a8d7e0f84debfc5f3d0b779b1d938ad1ccb
jaal9709 <jaal9709@gmail.com>
2024-02-17 12:21:07 -0500

    Solucionados algunos errores al conectar a la api

--- src/api/task.ts
+++ src/api/task.ts
@@ -1,22 +1,33 @@
-import React from "react";
 import { CreateTask, UpdateTask } from "../interfaces/task.interface";
 
-const API = "http://localhost:3000/api";
+const API = "http://3.19.72.172:3000/api";
 
 export const createTaskRequest = (task: CreateTask) =>
   fetch(`${API}/tasks`, {
     method: "POST",
     body: JSON.stringify(task),
     headers: {
+      "Access-Control-Allow-Origin": '*',
       "Content-Type": "application/json",
     },
   });
 
-export const getTaskRequest = () => fetch(`${API}/tasks`);
+export const getTaskRequest = () => 
+fetch(`${API}/tasks`,
+{
+  method: "GET",
+  headers:{
+    "Access-Control-Allow-Origin": '*',
+  }
+}
+);
 
 export const deleteTaskRequest = (id: string) =>
   fetch(`${API}/tasks/${id}`, {
     method: "DELETE",
+    headers:{
+      "Access-Control-Allow-Origin": '*',
+    }
   });
 
 export const updateTaskRequest = (id: string, task: UpdateTask) =>
@@ -24,6 +35,7 @@ export const updateTaskRequest = (id: string, task: UpdateTask) =>
     method: "PUT",
     body: JSON.stringify(task),
     headers: {
+      "Access-Control-Allow-Origin": '*',
       "Content-Type": "application/json",
     },
   });
